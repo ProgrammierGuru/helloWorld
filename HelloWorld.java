6062460f991483b8d523558cc2cdc88c36ba8585
D4M14N <btccool289@gmail.com>
2020-10-25 21:35:51 +0100

    feature B - new Attribut with Getters/Setters

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,9 +4,12 @@ public class HelloWorld {
 
 	private int i;
 
+	private String text;
+
 	public HelloWorld()
 	{
 		this.i = 0;
+		this.text = "Hallo";
 	}
 
 	public int getI()
@@ -18,4 +21,15 @@ public class HelloWorld {
 	{
 		this.i = i;
 	}
+
+	public void setText(String text)
+	{
+		this.text = text;
+	}
+
+	public String getText()
+	{
+		return this.text;
+	}
+
 }
